38576d6168671df0f7d70389e92b3f6fc2a68786
Ikramico <67937289+Ikramico@users.noreply.github.com>
2021-01-22 15:04:34 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,27 @@
+//5
+let ofiicejawariccha = false;
+let basaymangsoache = false;
+let kikorbo = (ofiicejawariccha == true) ? "chata ber korbo" : "mail korbo ami osustho" ;
+console.log(kikorbo);
+let kikhabo = (basaymangsoache == true) && (ofiicejawariccha == false) ? "khichuri mangso r dim vaji khabo" : "khichuri r dim vaji khabo" ;
+console.log(kikhabo);
+//3
+function building(a, b, c){
+    console.log(a*15*1000 + b*12*1000 + c*10*1000);
+}
+building(10, 10, 2);
+
+//1
+function InchToFeet(i){
+    console.log(f = i*.083);
+}
+InchToFeet(25);
+function FeetToMile(m){
+    console.log(m = f*0.00019);
+}
+FeetToMile(f);
+//2
+function Kath(ta, ch, kh){
+    console.log(ta*3 + ch*1 +  kh*5  );
+}
+Kath(2, 5, 1);
